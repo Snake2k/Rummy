85d5590a704a94f92be6240244080a645e0be190
Anonymous <Snake2k@localhost.localdomain>
2015-01-11 04:17:36 -0500

    Exit and About Actions

--- Rummy/gui/RGUI.py
+++ Rummy/gui/RGUI.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- 
 
 ###########################################################################
 ## Python code generated with wxFormBuilder (version Jul  5 2013)
@@ -14,84 +14,84 @@ import wx
 ###########################################################################
 
 class RMainFrame ( wx.Frame ):
-
-    def __init__( self, parent ):
-        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Rummy", pos = wx.DefaultPosition, size = wx.Size( 800,600 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
-
-        self.SetSizeHintsSz( wx.Size( 800,600 ), wx.Size( 800,600 ) )
-
-        RMainBSizer = wx.BoxSizer( wx.VERTICAL )
-
-        self.SetSizer( RMainBSizer )
-        self.Layout()
-        self.RStatusBar = self.CreateStatusBar( 1, wx.ST_SIZEGRIP, wx.ID_ANY )
-        self.RMenuBar = wx.MenuBar( 0 )
-        self.RFileMenu = wx.Menu()
-        self.RMItemNew = wx.MenuItem( self.RFileMenu, wx.ID_ANY, u"New", wx.EmptyString, wx.ITEM_NORMAL )
-        self.RFileMenu.AppendItem( self.RMItemNew )
-
-        self.RMItemSave = wx.MenuItem( self.RFileMenu, wx.ID_ANY, u"Save", wx.EmptyString, wx.ITEM_NORMAL )
-        self.RFileMenu.AppendItem( self.RMItemSave )
-
-        self.RFileMenu.AppendSeparator()
-
-        self.RMItemQuit = wx.MenuItem( self.RFileMenu, wx.ID_ANY, u"Quit", wx.EmptyString, wx.ITEM_NORMAL )
-        self.RFileMenu.AppendItem( self.RMItemQuit )
-
-        self.RMenuBar.Append( self.RFileMenu, u"File" )
-
-        self.RMHelpMenu = wx.Menu()
-        self.RMItemAbout = wx.MenuItem( self.RMHelpMenu, wx.ID_ANY, u"About"+ u"\t" + u"F1", wx.EmptyString, wx.ITEM_NORMAL )
-        self.RMHelpMenu.AppendItem( self.RMItemAbout )
-
-        self.RMenuBar.Append( self.RMHelpMenu, u"Help" )
-
-        self.SetMenuBar( self.RMenuBar )
-
-
-        self.Centre( wx.BOTH )
-
-        # Connect Events
-        self.Bind( wx.EVT_MENU, self.__close_frame, id = self.RMItemQuit.GetId() )
-        self.Bind( wx.EVT_MENU, self.__show_about, id = self.RMItemAbout.GetId() )
-
-    def __del__( self ):
-        pass
-
-
-    # Virtual event handlers, overide them in your derived class
-    def __close_frame( self, event ):
-        event.Skip()
-
-    def __show_about( self, event ):
-        event.Skip()
-
+	
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Rummy", pos = wx.DefaultPosition, size = wx.Size( 800,600 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		
+		self.SetSizeHintsSz( wx.Size( 800,600 ), wx.Size( 800,600 ) )
+		
+		RMainBSizer = wx.BoxSizer( wx.VERTICAL )
+		
+		self.SetSizer( RMainBSizer )
+		self.Layout()
+		self.RStatusBar = self.CreateStatusBar( 1, wx.ST_SIZEGRIP, wx.ID_ANY )
+		self.RMenuBar = wx.MenuBar( 0 )
+		self.RFileMenu = wx.Menu()
+		self.RMItemNew = wx.MenuItem( self.RFileMenu, wx.ID_ANY, u"New", wx.EmptyString, wx.ITEM_NORMAL )
+		self.RFileMenu.AppendItem( self.RMItemNew )
+		
+		self.RMItemSave = wx.MenuItem( self.RFileMenu, wx.ID_ANY, u"Save", wx.EmptyString, wx.ITEM_NORMAL )
+		self.RFileMenu.AppendItem( self.RMItemSave )
+		
+		self.RFileMenu.AppendSeparator()
+		
+		self.RMItemQuit = wx.MenuItem( self.RFileMenu, wx.ID_ANY, u"Quit", wx.EmptyString, wx.ITEM_NORMAL )
+		self.RFileMenu.AppendItem( self.RMItemQuit )
+		
+		self.RMenuBar.Append( self.RFileMenu, u"File" ) 
+		
+		self.RMHelpMenu = wx.Menu()
+		self.RMItemAbout = wx.MenuItem( self.RMHelpMenu, wx.ID_ANY, u"About"+ u"\t" + u"F1", wx.EmptyString, wx.ITEM_NORMAL )
+		self.RMHelpMenu.AppendItem( self.RMItemAbout )
+		
+		self.RMenuBar.Append( self.RMHelpMenu, u"Help" ) 
+		
+		self.SetMenuBar( self.RMenuBar )
+		
+		
+		self.Centre( wx.BOTH )
+		
+		# Connect Events
+		self.Bind( wx.EVT_MENU, self.close_frame, id = self.RMItemQuit.GetId() )
+		self.Bind( wx.EVT_MENU, self.show_about, id = self.RMItemAbout.GetId() )
+	
+	def __del__( self ):
+		pass
+	
+	
+	# Virtual event handlers, overide them in your derived class
+	def close_frame( self, event ):
+		event.Skip()
+	
+	def show_about( self, event ):
+		event.Skip()
+	
 
 ###########################################################################
 ## Class RGamePanel
 ###########################################################################
 
 class RGamePanel ( wx.Panel ):
-
-    def __init__( self, parent ):
-        wx.Panel.__init__ ( self, parent, id = wx.ID_ANY, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.TAB_TRAVERSAL )
-
-
-    def __del__( self ):
-        pass
-
+	
+	def __init__( self, parent ):
+		wx.Panel.__init__ ( self, parent, id = wx.ID_ANY, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.TAB_TRAVERSAL )
+		
+	
+	def __del__( self ):
+		pass
+	
 
 ###########################################################################
 ## Class RMainMenuPanel
 ###########################################################################
 
 class RMainMenuPanel ( wx.Panel ):
-
-    def __init__( self, parent ):
-        wx.Panel.__init__ ( self, parent, id = wx.ID_ANY, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.TAB_TRAVERSAL )
-
-
-    def __del__( self ):
-        pass
-
+	
+	def __init__( self, parent ):
+		wx.Panel.__init__ ( self, parent, id = wx.ID_ANY, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.TAB_TRAVERSAL )
+		
+	
+	def __del__( self ):
+		pass
+	
 
--- Rummy/gui/RGameBoard.py
+++ Rummy/gui/RGameBoard.py
@@ -4,24 +4,15 @@ Derived Classes for RGUI
 import wx
 import RGUI
 
-# pylint: disable=R0904
 class RGameBoard(RGUI.RMainFrame):
     '''
     Derived Class of RMainFrame which will construct the game board.
     '''
     def __init__(self, parent):
         RGUI.RMainFrame.__init__(self, parent)
-        self.RGameBoard = RGUI.RGamePanel(self)
-        self.RGameBoard.SetFocus()
-        self.Centre()
-        self.Show(True)
-
-    def __close_frame(self, event):
-        print "Wah"
+    def close_frame(self, event):
         self.Close()
-        event.Skip()
-    def __show_about(self, event):
-        print "Wahwah?!"
+    def show_about(self, event):
         wx.MessageBox("Working on it Jesus F***in' Christ..." + \
-                      "Brought to you by puddlejumper and Snake2k!.",
+                      "Brought to you by puddlejumper and Snake2k!.", "About",
                       wx.OK | wx.ICON_INFORMATION)
